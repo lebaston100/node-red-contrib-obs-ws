270053f6936b9e5f7c28cef572309ee159ff20dc
lebaston100 <26111114+lebaston100@users.noreply.github.com>
2022-09-18 22:18:50 +0200

    Add all request helper script

---
+++ tools/generateAllRequests.py
@@ -0,0 +1,38 @@
+import asyncio
+import aiohttp
+import logging
+import json
+
+"""Generate the available obs types and format them as a javascript array"""
+
+async def loadGithubJson() -> None:
+    url = "https://github.com/obsproject/obs-websocket/raw/master/docs/generated/protocol.json"
+    response = None
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                response = await resp.json(content_type="text/plain")
+    except Exception as E:
+        raise E
+    else:
+        #print(json.dumps(response["requests"], indent=2, sort_keys=True))
+
+        charsInLine = 0
+        outputText = "const websocketAllRequestTypes = [\n\t"
+        for event in response["requests"]:
+            requestType = event["requestType"]
+            requestFields = event["requestFields"]
+            if charsInLine > 80:
+                charsInLine = 0
+                outputText += "\n\t"
+            outputText += f'"{requestType}", '
+            charsInLine += len(requestType)
+        outputText += "\n];"
+        print(outputText)
+
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+loop = asyncio.get_event_loop()
+try:
+    loop.run_until_complete(loadGithubJson())
+finally:
+    loop.close()
